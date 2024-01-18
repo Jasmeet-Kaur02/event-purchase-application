d3ecb8381863d8ee95ea4ed720a1182e771c399e
JasmeetKaurx <jasskaur.dha@gmail.com>
2024-01-18 08:31:00 +0530

    added required fields in users model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phoneNumber')->nullable();
+            $table->boolean('isVerified')->default(false);
+            $table->string('stripeId')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
